5facd81ce57598bfea81d9620848ffcc7a32ce59
rahim2011 <alirahimalizade@gmail.com>
2025-02-08 16:50:29 +0400

    creating spring project

---
+++ codes/spring/spring-project-2sentyabr/src/main/java/az/developia/spring_project_2sentyabr/SpringProject2sentyabrApplication.java
@@ -0,0 +1,13 @@
+package az.developia.spring_project_2sentyabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject2sentyabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject2sentyabrApplication.class, args);
+	}
+
+}
---
+++ codes/spring/spring-project-2sentyabr/src/test/java/az/developia/spring_project_2sentyabr/SpringProject2sentyabrApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.spring_project_2sentyabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject2sentyabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
